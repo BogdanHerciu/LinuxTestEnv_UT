923f7b28b77ecb7e4916b4583fc631052ced12d0
BogdanHerciu <bogdan_herciu@yahoo.com>
2024-12-13 17:07:41 +0200

    Create main_file.py

---
+++ main_file.py
@@ -0,0 +1,17 @@
+import random
+
+#################################
+# # # # # # Main file # # # # # # 
+#################################
+
+def func_1():
+	''' This function should return a random number between 9 and 28 '''
+	return random.randint(9, 28)
+def sum_of_two_integers(a,b):
+	''' This function returns the sum of a and b'''
+	return a+b
+
+# Protect executable code with this block
+if __name__ == "__main__":
+    print(func_1())
+    print(sum_of_two_integers(6, 8))
