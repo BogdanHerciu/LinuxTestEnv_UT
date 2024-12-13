c690d0815ef3edd920e3133ab645385becc37159
BogdanHerciu <bogdan_herciu@yahoo.com>
2024-12-13 16:46:55 +0200

    update_2

--- test_UTtest.py
+++ test_UTtest.py
@@ -1,5 +1,4 @@
-import main
-
+import main_file
 
 #############################################
 # TC01:
@@ -7,8 +6,8 @@ import main
 #############################################
 def UT_test_1_func_1():
     for crt_idx in range(1000):
-        result = func_1()
-        assert (not (9 <= result <= 28)), "Error: function returns values out of bounds"
+        result = main_file.func_1()
+        assert (9 <= result <= 28), "Error: function returns values out of bounds"
     print("TC01 ended")
 
 #############################################
@@ -16,5 +15,10 @@ def UT_test_1_func_1():
 # target function: sum_of_two_integers
 #############################################
 def UT_test_2_sum_of_two_integers():
-    result = sum_of_two_integers(5,6)
+    result = main_file.sum_of_two_integers(5,6)
     assert result == 11, f"Expected 3, but got {result}"
+    print("TC02 ended")
+
+
+UT_test_1_func_1()
+UT_test_2_sum_of_two_integers()
