8141881dd30856098d0ec92cd5e0a380c77aee05
BogdanHerciu <bogdan_herciu@yahoo.com>
2024-12-13 17:11:05 +0200

    Update test_UTtest.py

--- test_UTtest.py
+++ test_UTtest.py
@@ -1,10 +1,19 @@
 import main_file
 
 
+'''
+Test Case Description: Testing func_1
+Test function        : func_1
+'''
 def test_1():
-    result = 3
-    assert result == 3, f"Expected 3, but got {result}"
+    for crtIdx in range(1000):
+        result = func_1()
+        assert (9 <= result <= 28), "Error: function returns values out of bounds"
 
+'''
+Test Case Description: Testing sum_of_two_integers
+Test function        : sum_of_two_integers
+'''
 def test_2():
     result = 0
     assert result == 0, f"Expected 0, but got {result}"
