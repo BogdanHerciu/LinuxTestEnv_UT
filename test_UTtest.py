ec77128bcd73e19d899222effd03220adfc292ae
BogdanHerciu <bogdan_herciu@yahoo.com>
2024-12-13 17:05:21 +0200

    Update test_UTtest.py

--- test_UTtest.py
+++ test_UTtest.py
@@ -1,12 +1,6 @@
 def add(a,b):
     return a+b
 
-def asd():
-    # Test if add(1, 2) returns 3
+def UT_test_1_func_1():
     result = add(1, 2)
     assert result == 3, f"Expected 3, but got {result}"
-
-def xcv():
-    # Test if add(-1, 1) returns 0
-    result = add(-1, 1)
-    assert result == 0, f"Expected 0, but got {result}"
