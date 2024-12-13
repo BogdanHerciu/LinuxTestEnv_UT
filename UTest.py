75eb13b9537b1e442dc0b7b0f2852190a8c92925
BogdanHerciu <bogdan_herciu@yahoo.com>
2024-12-13 13:37:19 +0200

    Update UTest.py

--- UTest.py
+++ UTest.py
@@ -5,8 +5,11 @@ def test_2():
     print("this is function 2")
 def test_3():
     print("this is function 3")
-
+def test_4():
+    print("this is function 4")
 test_1()
 test_2()
+test_3()
+test_4()
 #test comment
 #test comment 2
